a5c6b066ba43e74b12991ba07412cda76cb608bf
Noskvik <81382625+Noskvik@users.noreply.github.com>
2023-09-08 21:29:51 +0400

    Create main.php

---
+++ Web/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+
+<html lang="ru">
+
+<head>
+    <title>CodeCaeser</title>
+    <meta charset = "UTF-8">
+    <!-- <link rel = "stylesheet" href = "style.css"> -->
+</head>
+<body>
+  <div class = "flex-container">
+    <div class = "head-page">
+    </div>
+    <div class = "main-page">
+    </div>
+    <div class = "footer-page">
+    </div>
+  </div>
+</body>
