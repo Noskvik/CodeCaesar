3523888d4f305bc0ad452c22d4f6789cba9917fc
Noskvik <81382625+Noskvik@users.noreply.github.com>
2023-09-28 14:38:54 +0400

    Update main.php

--- Project/Web/main.php
+++ Project/Web/main.php
@@ -1,1 +1,45 @@
+<!DOCTYPE HTML>
 
+<html lang="ru">
+
+<head>
+  <title>CodeCaesar</title>
+  <meta charset = "UTF-8">
+  <link rel = "stylesheet" href = "style.css">
+  <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+  <py-env>
+      - paths:
+          - ../DefCodeCaesar.py
+          - ../EngReview.txt
+          - ../RusReview.txt
+  </py-env>
+</head>
+
+<body>
+  <div class = "flex-container">
+    <div class = "head-page">
+      <img src="Label.png" width = "600" alt = "Логотип">
+    </div>
+    <div class = "main-page">
+      <div class = "rowText">
+        <p>Entered text:&nbsp;<p id = "inputText"></p></p>
+      </div>
+      <py-script id = "pyResult"> 
+          from DefCodeCaesar import coder_caesar
+          lang, rev, *text = input().split()
+          lang = str(lang)
+          rev = str(rev)
+          resultText = ''
+          for num in range(len(text)):
+              resultText += text[num]
+              if num == len(text)-1:
+                continue
+              resultText += " "
+          coder_caesar(lang, rev, resultText)
+          pyscript.write('inputText', resultText)
+      </py-script>
+    </div>
+    <div class = "footer-page">
+    </div>
+  </div>
+</body>
