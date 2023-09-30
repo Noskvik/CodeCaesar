59005f4f2925b42a28bc8cae1bab6b2b18530fb4
Noskvik <81382625+Noskvik@users.noreply.github.com>
2023-09-30 17:44:08 +0400

    Update CodeCaesar.py

--- Project/CodeCaesar.py
+++ Project/CodeCaesar.py
@@ -8,10 +8,22 @@ parser.add_argument('text', help="Text to be processed", type=str, nargs='+')
 args = parser.parse_args()
 
 language = str(args.language)
-review = str(args.review)
+if args.review == 'y':
+    review = True
+elif args.review == 'n':
+    review = False
+else: 
+    review = 'Error'
 text = list(args.text)
 resultText = ""
 for x in text:
     resultText += f"{x} "
 
-coder_caesar(language, review, resultText)
+listProcess = coder_caesar(language, review, resultText)
+
+if listProcess[0] is True:
+    print(listProcess[1])
+else:
+    for elem in listProcess:
+        print(elem)
+    print("The process is completed!")
